5b2c77ae6aac9d92619df483d12f5144b77a22f8
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-10-21 18:06:11 -0700

    Added additional pseudocode

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Array list of numbers
+// DELCARE a list of `numbers`
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Array list of uppercase numbers
-["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//DECLARE a list of `lowercaseLetters`
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// Array list of lower numbers
+// DECLARE a list of `uppercaseLetters`
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-// Array list of special characters
+// DECLARE a list of `specialCharaters`
+var specialCharacters = []
 
 // Could put the above into one object, rather than in an array
 
 //Store variables as you are prompting the user for information.
 
 function generatePassword {
-// Password length var
+// PROMPT the user for a `passwordLength`
   const passwordLength = prompt("How many characters would you like your password to have?")
-// Confirm user number
 
-// Confrim use lowercase letters
+if (!(passworkdLength >=8 && passwordLEngth <= 128)) {
+  // THEN ALERT to the user that they need to provide a correct length
+  alert("Please use a password length between 8 and 128 characters.")
+  // AND EXIT FUNCTION 
+  return;
 
-// Confirm use uppercase letters
-
-// Confirm use special characters
+}
+// CONFIRM if the passworkd genereator `isUsingNumbers`
+// CONFIRM if the password genreator `isUsingLowercaseLetters`
+// CONFIRM if the password generator `isUsingUppercase Letters`
+// CONFIRM if the password generator `isUsingSpecialCharactersLetters`
 
 }
 
