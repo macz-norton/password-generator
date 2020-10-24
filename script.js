f690fa77e4df31930314b53cbf8feb93f411c1ca
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-10-24 03:12:55 -0700

    Updated first alert and event listener for button click

--- script.js
+++ script.js
@@ -1,41 +1,7 @@
-// DECLARE a `generateBtn`
-var generateBtn = document.querySelector("#generate");
-
-// DECLARE a list of `numbers`
-var numbers = "0123456789".split("");
-console.log(numbers);
-
-// DECLARE a list of `lowercaseLetters`
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(lowercaseLetters);
-
-// DECLARE a list of `uppercaseLetters`
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-console.log(uppercaseLetters);
-
-// // DECLARE a list of `specialCharaters`
-var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
-console.log(specialCharacters);
-
-// //Store variables as you are prompting the user for information.
-
-// function generatePassword {
-// // PROMPT the user for a `passwordLength`
-//   const passwordLength = prompt("How many characters would you like your password to have?")
-
-// if (!(passworkdLength >=8 && passwordLEngth <= 128)) {
-//   // THEN ALERT to the user that they need to provide a correct length
-//   alert("Please use a password length between 8 and 128 characters.")
-//   // AND EXIT FUNCTION 
-//   return;
-
-// }
 // // CONFIRM if the passworkd genereator `isUsingNumbers`
 // // CONFIRM if the password genreator `isUsingLowercaseLetters`
 // // CONFIRM if the password generator `isUsingUppercase Letters`
 // // CONFIRM if the password generator `isUsingSpecialCharactersLetters`
-
-// }
 
 // // DECLARE a new `password` string
 // var password = ""
@@ -52,8 +18,7 @@ console.log(specialCharacters);
 
 // }
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+
 
 // ${password}
 
@@ -71,4 +36,42 @@ console.log(specialCharacters);
 // // WHEN all prompts are answered
 // // THEN a password is generated that matches the selected criteria
 // // WHEN the password is generated
-// // THEN the password is either displayed in an alert or written to the page
+// THEN the password is either displayed in an alert or written to the page
+
+
+// DECLARE a `generateBtn`
+var generateBtn = document.querySelector("#generate");
+
+// DECLARE a list of `numbers`
+var numbers = "0123456789".split("");
+console.log(numbers);
+// DECLARE a list of `lowercaseLetters`
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+console.log(lowercaseLetters);
+// DECLARE a list of `uppercaseLetters`
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+console.log(uppercaseLetters);
+// // DECLARE a list of `specialCharaters`
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+console.log(specialCharacters);
+
+// STORE variables as you are prompting the user for information.
+var password = "";
+console.log(password);
+
+var writePassword = [];
+
+function generatePassword() {
+// PROMPT the user for a `passwordLength`
+  const passwordLength = parseInt(prompt("How many characters would you like your password to have?"));
+  console.log(passwordLength);
+
+  if (!(passwordLength >=8 && passwordLength <= 128)) {
+  // THEN ALERT to the user that they need to provide a correct length
+  alert("Please use a password length between 8 and 128 characters.")
+  // EXIT function
+  return;
+}
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
