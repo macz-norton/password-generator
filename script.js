692e8b1421c2355caa8f879158482557499e1159
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-10-24 14:57:21 -0700

    Updated special characters, started using for loop

--- script.js
+++ script.js
@@ -14,22 +14,17 @@ function writePassword() {
 
 // DECLARE a list of `numbers`
 var numbers = "0123456789".split("");
-console.log(numbers);
 // DECLARE a list of `lowercaseLetters`
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(lowercaseLetters);
 // DECLARE a list of `uppercaseLetters`
 var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-console.log(uppercaseLetters);
-// // DECLARE a list of `specialCharaters`
-var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
-console.log(specialCharacters);
+// // DECLARE a list of `specialCharacters`
+var specialCharacters = ["!","#","$","%","&","*","+","-",".","<",">","?","@","^","_","~"];
 
 function generatePassword() {
 
 // PROMPT the user for a `passwordLength`
   var passwordLength = parseInt(prompt("Choose a password length between 8 and 128 characters."));
-  console.log(passwordLength);
 
   // IF password is out of length boundary of 8-128 characters
   if ( !(passwordLength >=8 || passwordLength <= 128) ) {
@@ -40,52 +35,42 @@ function generatePassword() {
   }
 
   // DECLARE a new list of `charactersToUse`
-  var charactersToUse = [];
-  console.log(specialCharacters);
+  var charactersToUse = [""];
 
   // DECLARE new `password` string 
   var password = "";
 
   // CONFIRM if the password generator `includesNumbers`
   var includesNumbers = confirm("Do you want to include numbers? If yes, click 'Okay.'");
-  console.log(includesNumbers);
 
   // IF 'okay', THEN add numbers to `charactersToUse`
   if ( includesNumbers == true ) {
     charactersToUse = charactersToUse.concat(numbers);
   }
-  console.log(charactersToUse);
 
   // CONFIRM if the password generator `includesLowercase`
   var includesLowercase = confirm("Do you want to include lowercase characters? If yes, click 'Okay.'");
-  console.log(includesLowercase);
 
   // IF 'okay', THEN add lowercase letters to `charactersToUse`
   if ( includesLowercase == true ) {
     charactersToUse = charactersToUse.concat(lowercaseLetters);
   }
-  console.log(charactersToUse);
-
 
   // CONFIRM if the password generator `includesUppercase`
   var includesUppercase = confirm("Do you want to include uppercase characters? If yes, click 'Okay.'");
-  console.log(includesUppercase);
 
     // IF 'okay', THEN add uppercase letters to `charactersToUse`
   if ( includesUppercase == true ) {
     charactersToUse = charactersToUse.concat(uppercaseLetters);
   }
-  console.log(charactersToUse);
 
   // CONFIRM if the password generator `includesSpecialCharacters`
   var includesSpecialCharacters = confirm("Do you want to include special characters? If yes, click 'Okay.'");
-  console.log(includesSpecialCharacters);
 
   // IF 'okay', THEN add special characters to `charactersToUse`
   if ( includesSpecialCharacters == true ) {
     charactersToUse = charactersToUse.concat(specialCharacters);
   }
-  console.log(charactersToUse);
 
   // IF user selected no characters
   if ( charactersToUse.length = 0 ) {
@@ -95,18 +80,18 @@ function generatePassword() {
     return;
   }
 
-  
-   console.log(passwordLength);  
-
-   var randomIndex = "";
-   var newCharacter = "";
-  // WHILE the `password.length` < `passwordLength`
-  while ( password.length < passwordLength ) {
+  var randomIndex;
+  var newCharacter;
+  for (var i = 0; i < passwordLength; i++) {
     // SELECT `randomCharacter` from `charactersToUse`
     randomIndex = Math.floor(Math.random() * charactersToUse.length);
     newCharacter = charactersToUse[randomIndex];
-    // APPEND `newCharacter` to `password`
     password += newCharacter;
+
+    console.log(charactersToUse);
+    console.log(randomIndex);
+    console.log(newCharacter);
+    console.log(password);
   }
-  return password;
+    return password;
 }
