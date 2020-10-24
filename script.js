e0bf88fbb81ec4225393bdf1b4ecb468fc2b7122
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-10-24 06:22:49 -0700

    Re-arranged order, updated while loop, console.logs

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-
-
-// // DECLARE a new `password` string
-// var password = ""
-// //WHILE passowrd.length < passwordLength
-// while( password.Length < passworkdLEngth ) {}
-
-
-
-
-
-
-// ${password}
-
-
-// // THEN I am presented with a series of prompts for password criteria
-// // WHEN prompted for password criteria
-// // THEN I select which criteria to include in the password
-// // WHEN prompted for the length of the password
-// // THEN I choose a length of at least 8 characters and no more than 128 characters
-// // WHEN prompted for character types to include in the password
-// // THEN I choose lowercase, uppercase, numeric, and/or special characters
-// // WHEN I answer each prompt
-// // THEN my input should be validated and at least one character type should be selected
-// // WHEN all prompts are answered
-// // THEN a password is generated that matches the selected criteria
-// // WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
 // DECLARE a `generateBtn`
 var generateBtn = document.querySelector("#generate");
+
+// WHEN I click the button to generate a password
+generateBtn.addEventListener("click", writePassword);
+
+// PASTE `password` to `passwordText`
+function writePassword() {
+  var newPassword = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = newPassword;
+}
 
 // DECLARE a list of `numbers`
 var numbers = "0123456789".split("");
@@ -44,68 +25,88 @@ console.log(uppercaseLetters);
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
 console.log(specialCharacters);
 
-// STORE variables as you are prompting the user for information.
-var charactersToUse = [];
-
-var password = "";
-console.log(password);
-
 function generatePassword() {
 
 // PROMPT the user for a `passwordLength`
-  const passwordLength = parseInt(prompt("Choose a password length between 8 and 128 characters."));
+  var passwordLength = parseInt(prompt("Choose a password length between 8 and 128 characters."));
   console.log(passwordLength);
 
-  if (!(passwordLength >=8 || passwordLength <= 128)) {
-  // THEN ALERT to the user that they need to provide a correct length
+  // IF password is out of length boundary of 8-128 characters
+  if ( !(passwordLength >=8 || passwordLength <= 128) ) {
+    // THEN ALERT to the user that they need to provide a correct length
     alert("Please use a password length between 8 and 128 characters.")
-  // EXIT function
-  return;
-  }
-  // CONFIRM if the password generator `includesNumbers`
-  const includesNumbers = confirm("Do you want to include numbers? If yes, click 'Okay.'");
-  console.log(includesNumbers);
-
-  if (includesNumbers == true) {
-    charactersToUse.concat(numbers);
-  }
-  // CONFIRM if the password generator `includesLowercase`
-  const includesLowercase = confirm("Do you want to include lowercase characters? If yes, click 'Okay.'");
-  console.log(includesLowercase);
-
-  if (includesLowercase == true) {
-    charactersToUse.concat(lowercaseLetters);
-  }
-
-  // CONFIRM if the password generator `includesUppercase`
-  const includesUppercase = confirm("Do you want to include uppercase characters? If yes, click 'Okay.'");
-  console.log(includesUppercase);
-
-  if (includesUppercase == true) {
-    charactersToUse.concat(uppercaseLetters);
-  }
-
-  // CONFIRM if the password generator `includesSpecialCharacters`
-  const includesSpecialCharacters = confirm("Do you want to include special characters? If yes, click 'Okay.'");
-  console.log(includesSpecialCharacters);
-
-  if (includesSpecialCharacters == true) {
-    charactersToUse.concat(specialCharacters);
-  }
-
-  else {
+    // EXIT function
     return;
   }
 
+  // DECLARE a new list of `charactersToUse`
+  var charactersToUse = [];
+  console.log(specialCharacters);
+
+  // DECLARE new `password` string 
+  var password = "";
+
+  // CONFIRM if the password generator `includesNumbers`
+  var includesNumbers = confirm("Do you want to include numbers? If yes, click 'Okay.'");
+  console.log(includesNumbers);
+
+  // IF 'okay', THEN add numbers to `charactersToUse`
+  if ( includesNumbers == true ) {
+    charactersToUse = charactersToUse.concat(numbers);
+  }
+  console.log(charactersToUse);
+
+  // CONFIRM if the password generator `includesLowercase`
+  var includesLowercase = confirm("Do you want to include lowercase characters? If yes, click 'Okay.'");
+  console.log(includesLowercase);
+
+  // IF 'okay', THEN add lowercase letters to `charactersToUse`
+  if ( includesLowercase == true ) {
+    charactersToUse = charactersToUse.concat(lowercaseLetters);
+  }
+  console.log(charactersToUse);
+
+
+  // CONFIRM if the password generator `includesUppercase`
+  var includesUppercase = confirm("Do you want to include uppercase characters? If yes, click 'Okay.'");
+  console.log(includesUppercase);
+
+    // IF 'okay', THEN add uppercase letters to `charactersToUse`
+  if ( includesUppercase == true ) {
+    charactersToUse = charactersToUse.concat(uppercaseLetters);
+  }
+  console.log(charactersToUse);
+
+  // CONFIRM if the password generator `includesSpecialCharacters`
+  var includesSpecialCharacters = confirm("Do you want to include special characters? If yes, click 'Okay.'");
+  console.log(includesSpecialCharacters);
+
+  // IF 'okay', THEN add special characters to `charactersToUse`
+  if ( includesSpecialCharacters == true ) {
+    charactersToUse = charactersToUse.concat(specialCharacters);
+  }
+  console.log(charactersToUse);
+
+  // IF user selected no characters
+  if ( charactersToUse.length = 0 ) {
+    // THEN ALERT to the user that they need to select character
+    alert("Please select one or more types of characters.")
+    // EXIT function
+    return;
+  }
+
+  var newCharacter;
+
+  // WHILE the `password.length` < `passwordLength`
+  while( password.Length < passwordLength ) {
+    // SELECT `randomCharacter` from `charactersToUse`
+    var randomCharacter = charactersToUse[Math.floor(Math.random() * charactersToUse.length)];
+    // APPEND `randomCharacter` to `password`
+    var newPassword = 
+    newCharacter = charactersToUse[randomCharacter];
+    newPassword.push(newCharacter);
+  }
+  console.log(password);
+  console.log(charactersToUse);
+  return password;
 }
-
-// PASTE password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// WHEN I click the button to generate a password
-generateBtn.addEventListener("click", writePassword);
